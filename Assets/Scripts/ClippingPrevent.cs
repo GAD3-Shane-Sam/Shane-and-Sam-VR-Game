8ed663357a0625ee8130e7b3344cddf7f9f28f79
Shane MacNamara <117648302+tus-smacnamara-21@users.noreply.github.com>
2023-11-14 12:52:21 +0000

    Added Clipping Prevention

---
+++ Assets/Scripts/ClippingPrevent.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClippingPrevent : MonoBehaviour
+{
+    [SerializeField] LayerMask collisionLayer;
+    [SerializeField] float fadeSpeed;
+    [SerializeField] float sphereCheckSize = .15f;
+
+    private Material cameraFadeMat;
+    private bool isCameraFadeOut = false;
+
+    private void Awake()
+    {
+        cameraFadeMat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (Physics.CheckSphere(transform.position, sphereCheckSize, collisionLayer, QueryTriggerInteraction.Ignore))
+        {
+            CameraFade(1f);
+            isCameraFadeOut = true;
+        }
+        else
+        {
+            if (!isCameraFadeOut)
+            return;
+            
+                CameraFade(0f);
+        }
+    }
+    public void CameraFade(float targetAlpha)
+    {
+        var fadeValue = Mathf.MoveTowards(cameraFadeMat.GetFloat("_AlphaValue"), targetAlpha, Time.deltaTime * fadeSpeed);
+        cameraFadeMat.SetFloat("_AlphaValue", fadeValue);
+
+        if (fadeValue <= 0.01f)
+
+        isCameraFadeOut = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, sphereCheckSize);
+    }
+}
