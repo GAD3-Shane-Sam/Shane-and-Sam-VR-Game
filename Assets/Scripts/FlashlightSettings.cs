94a1971ec7fd65cd96d5f75e78a1f52f8e7fab82
Shane MacNamara <117648302+tus-smacnamara-21@users.noreply.github.com>
2023-11-21 12:58:26 +0000

    Flashlight Finished

--- Assets/Scripts/FlashlightSettings.cs
+++ Assets/Scripts/FlashlightSettings.cs
@@ -1,14 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FlashlightSettings : MonoBehaviour
 {
     public bool setting = false;
-    public bool lightSetting = true;
+    public bool lightSetting = false;
     public Light flashlight;
-    public bool selected;
+    public GameObject flashlightObject;
+    private XRGrabInteractable grabInteractable;
+    public bool isPickedUp = false;
 
     [SerializeField]
     private InputActionReference flashlightActionReference;
@@ -16,43 +18,107 @@ public class FlashlightSettings : MonoBehaviour
     [SerializeField]
     private InputActionReference flashlightSettingActionReference;
 
+    private bool isOn = false;
+    private float flashlightTimer = 0f;
+    private bool isRecharging = false;
+    private float rechargeTime = 5f; // Recharge time in seconds
+
     // Start is called before the first frame update
     void Start()
     {
-        flashlight = GetComponent<Light>();
         flashlightActionReference.action.performed += OnFlashlight;
-        flashlightSettingActionReference.action.performed -= OnFlashlightSetting;
+        flashlightSettingActionReference.action.performed += OnFlashlightSetting;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (setting)
+        flashlightObject.SetActive(setting);
+
+        if (isPickedUp && isOn)
         {
-            flashlight.intensity = 25;
+            flashlightTimer += Time.deltaTime;
+
+            if (flashlightTimer >= 10f) // Turn off after 10 seconds
+            {
+                TurnOffFlashlight();
+            }
         }
-        else
+
+        if (isPickedUp)
         {
-            flashlight.intensity = 0;
-        }
-        if (lightSetting)
-        {
-            flashlight.color = Color.white;
-            flashlight.intensity = 25;
-        }
-        else
-        {
-            flashlight.color = new Color(83, 0, 255);
+            if (!lightSetting)
+            {
+                flashlight.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                flashlight.color = new Color(0.325f, 0.0f, 1.0f, 1.0f);
+            }
         }
     }
 
     private void OnFlashlight(InputAction.CallbackContext obj)
     {
-        setting = !setting;
+        if (isPickedUp)
+        {
+            setting = !setting;
+            if (setting)
+            {
+                isOn = true;
+                flashlight.enabled = true;
+                flashlightTimer = 0f;
+            }
+            else
+            {
+                TurnOffFlashlight();
+            }
+        }
+    }
+
+    private void TurnOffFlashlight()
+    {
+        isOn = false;
+        flashlight.enabled = false;
+        flashlightTimer = 0f;
+        StartCoroutine(RechargeFlashlight());
     }
 
     private void OnFlashlightSetting(InputAction.CallbackContext obj)
     {
-        lightSetting = !lightSetting;
+        if (isPickedUp && !isRecharging)
+        {
+            lightSetting = !lightSetting;
+        }
+    }
+
+    private void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnObjectPickedUp);
+            grabInteractable.selectExited.AddListener(OnObjectReleased);
+        }
+    }
+
+    private void OnObjectPickedUp(SelectEnterEventArgs args)
+    {
+        isPickedUp = true;
+    }
+
+    private void OnObjectReleased(SelectExitEventArgs args)
+    {
+        isPickedUp = false;
+    }
+
+    private IEnumerator RechargeFlashlight()
+    {
+        if (!isRecharging)
+        {
+            isRecharging = true;
+            yield return new WaitForSeconds(rechargeTime);
+            isRecharging = false;
+        }
     }
 }
